47a2b705c4b3c261997d88bcd03728da0b49a742
muatasimqazi <muatasimqazi@gmail.com>
2017-06-26 15:32:24 -0700

    added templates

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+import cgi
+
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
@@ -7,43 +9,67 @@ form = """
     <html>
         <head>
             <title>User Signup</title>
+            <style>
+            #sign-up {{
+                width: 300px;
+                padding: 5px;
+            }}
+            label {{
+                float: left;
+                clear: left;
+            }}
+            input {{
+                float: right;
+                display: inline-block;
+            }}
+            #sign-up input, #sign-up label {{
+                margin-bottom: 5px;
+            }}
+            div #submit {{
+                float: left;
+                clear: left;
+            }}
+            </style>
         </head>
         <body>
 
         <h1>Signup</h1>
+          <div id="sign-up">
+              <form method="POST">
 
-          <form method="POST">
+              <div>
+                  <label for="rot">Username </label>
+                  <input type="text" name="username">
+              </div>
 
-          <div>
-              <label for="rot">Username </label>
-              <input type="text" name="username">
+              <div>
+                  <label for="rot">Password </label>
+                  <input type="password" name="password">
+              </div>
+
+              <div>
+                  <label for="rot">Verify Password </label>
+                  <input type="password" name="password">
+              </div>
+
+              <div>
+                  <label for="rot">Email (optional) </label>
+                  <input type="text" name="email">
+              </div>
+
+              <br>
+              <div id="submit">
+              <input type="submit">
+              </div>
+              </form>
+
           </div>
-
-          <div>
-              <label for="rot">Username </label>
-              <input type="text" name="username">
-          </div>
-
-          <div>
-              <label for="rot">Username </label>
-              <input type="text" name="username">
-          </div>
-
-          <div>
-              <label for="rot">Username </label>
-              <input type="text" name="username">
-          </div>
-
-          <input type="submit">
-          </form>
         </body>
     </html>
 """
 
 @app.route('/', methods=['POST'])
-def encrypt():
-    str_1 = request.form['rot']
-    str_2 = request.form['text']
+def signup():
     encrypted_str = rotate_string(str_2,int(str_1))
 
     return form.format(encrypted_str)
